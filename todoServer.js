b509b9eaf135b293ab7deb157f2b282f7c07bf05
kosterseb <sebkos.code@gmail.com>
2025-11-28 10:10:02 +0100

    Feat: implementing my MongoDB database to the express server

--- todoServer.js
+++ todoServer.js
@@ -1,10 +1,28 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-
+import mongoose from 'mongoose';
 
 const app = express();
 const PORT = 3000;
 app.use(bodyParser.json());
+
+const DB_URI = 'mongodb+srv://<db_username>:<db_password>@koestercluster.rqhqqyv.mongodb.net/'
+
+mongoose.connect(DB_URI)
+  .then(() => {
+    console.log('Successfully connected to MongoDB!');
+    // Start the Express server ONLY after a successful database connection
+    app.listen(port, () => {
+      console.log(`Server running on http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    // Log the error and exit the process if the connection fails
+    console.error('MongoDB connection error:', err.message);
+    process.exit(1);
+  });
+
+  // Middleware to parse JSON bodies
 
 let todos = [];
 app.get('/todos', (req, res) => {
